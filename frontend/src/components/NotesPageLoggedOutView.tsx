055fd594da12f1ef7a108b0d2f8e25fe2ab745c0
Ritik <ritik11181@gmail.com>
2024-04-18 23:54:08 +0530

    Fronted and Backend completed

---
+++ frontend/src/components/NotesPageLoggedOutView.tsx
@@ -0,0 +1,10 @@
+const NotesPageLoggedOutView = () => {
+    return (
+        <div className="privacyPage">
+            <p>Please login to see your notes.</p>
+        </div>
+        
+    );
+}
+
+export default NotesPageLoggedOutView;
---
+++ frontend/src/pages/PrivacyPage.tsx
@@ -0,0 +1,9 @@
+const PrivacyPage = () => {
+    return ( 
+        <div className="privacyPage">
+            <p>We care about your privacy. Promise!</p>
+        </div>
+     );
+}
+ 
+export default PrivacyPage;
